ca1aaf0f0df57e28d602013bc1ad1dfe5073c12c
Angel Del castillo <angelrap.1111@gmail.com>
2026-04-18 17:28:13 -0500

    Entrega parcial 2 - mission-app

--- medicare-visitas-ionic/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.medicare.visitas',
-  appName: 'medicare-visitas-ionic',
-  webDir: 'dist'
-};
-
-export default config;
